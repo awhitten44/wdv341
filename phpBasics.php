63a73bb78fa085a9457f48f0a87f83f4d3c16c4a
awhitten44 <54416364+awhitten44@users.noreply.github.com>
2019-09-20 23:18:52 -0500

    Add files via upload

---
+++ phpBasics.php
@@ -0,0 +1,22 @@
+<html>
+ <head>
+  <title>PHP Basics</title>
+ </head>
+ <body>
+ 
+<?php
+	$yourName = "Alex Whitten";
+?>
+
+<h1>Unit 2 - PHP Basics Assignment</h2>
+
+<h2><?php echo $yourName ?></h2>
+
+<?php
+$number1 = 12;
+$number2 = 8;
+echo $number1 + $number2;
+?> 
+ 
+  </body>
+</html>
