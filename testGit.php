56d15423090cbb7cf27977212c509c07380233dd
awhitten44 <54416364+awhitten44@users.noreply.github.com>
2019-09-20 22:35:02 -0500

    Add files via upload

--- testGit.php
+++ testGit.php
@@ -3,6 +3,10 @@
   <title>PHP Test</title>
  </head>
  <body>
+ 
  <?php echo '<p>Alex Whitten</p>'; ?> 
+ 
+<p> <a href="https://github.com/awhitten44/wdv341/blob/master/testGit.php">My GitHub Repository</a></p>
+ 
  </body>
 </html>
